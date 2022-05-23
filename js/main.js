266b8066028d87429f3ba98736988e6272bfe19a
Nicolas Vargas <nicolas.vargas1085@gmail.com>
2022-05-23 11:06:51 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+window.addEventListener("scroll", function () { //Detecnta el scroll y realiza la funcion.
+    let header = document.querySelector("nav");
+    header.classList.toggle("nav__abajo", window.scrollY > 0); //cada que vajemos un poco se realizara una clase al header
+})
+
+const lista = document.getElementById('carrucel__lista');
+
+Sortable.create(lista, {
+    animation: 150,
+    chosenClass: "pulsado",
+    ghostClass: "phantom",
+    dragClass: "arrastrar",
+    onEnd: () => {
+        console.log('Elemento Insertado');
+    }
+});
+
+
+
+// The location of google
+
+function initMap() {
+    const cord = { lat:4.6416877, lng: -74.1520707 };
+    // The map, centered at google
+    const map = new google.maps.Map(
+        document.getElementById("map"), {
+        zoom: 15,
+        center: cord,
+    });
+    const marker = new google.maps.Marker({
+        position: cord,
+        map: map,
+    });
+}
+
